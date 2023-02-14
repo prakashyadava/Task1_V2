574cffee0cfeab43ce2d8800851cc9e30e9ed087
Prakash Yadava <79514008+prakashyadava@users.noreply.github.com>
2023-02-14 17:11:21 +0530

    Update allConn.py

--- Connections/allConn.py
+++ Connections/allConn.py
@@ -17,16 +17,20 @@ class DBConnection:
             self.conn  = psycopg2.connect(**params)
             self.cur = self.conn.cursor()
             Log.log_connection(True)
-        except Exception as e:
-            print(e)
+            return True
+        except psycopg2.Error as e:
+            print(f'{e}')
+            return False
     
     def create_table(self,name,table):
         try:
             self.cur.execute(f"create table if not exists {name}(tid serial primary key,{table}) ;")
             self.conn.commit()
             Log.log_read()
-        except Exception as e:
-            print(e)
+            return True
+        except psycopg2.Error as e:
+            print(f'{e}')
+            return False
     def display_records(self,name):
         try:
             self.cur.execute(f"select * from {name} ;")
@@ -41,8 +45,8 @@ class DBConnection:
             self.cur.execute(f"insert into {name} {field} values ({data});")
             self.conn.commit()
             Log.log_insert()
-        except Exception as e:
-            print(e)
+        except psycopg2.Error as e:
+            print(f'{e}')
 
     #inserting data through executemany
     def insert_data_executemany(self,insert_query,tup):
@@ -50,8 +54,8 @@ class DBConnection:
             self.cur.executemany(f"{insert_query}", tup)
             self.conn.commit()
             Log.log_insert()
-        except Exception as e:
-            print(e)
+        except psycopg2.Error as e:
+            print(f'{e}')
     #inserting data through mogrify
     def insert_data_mogrify(self,table_name,field,x,tup):
         try:
@@ -59,12 +63,11 @@ class DBConnection:
             self.cur.execute(f"insert into {table_name} {field} values {mogrify_values}")
             self.conn.commit()
             Log.log_insert()
-        except Exception as e:
-            print(e)
-    
+        except psycopg2.Error as e:
+            print(f'{e}')
     def close_db(self):
         try:
             self.conn.close()
             Log.log_close(True)
-        except Exception as e:
-            print(e)
+        except psycopg2.Error as e:
+            print(f'{e}')
